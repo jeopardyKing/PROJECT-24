f613c0f62a3b086465af122f95f3ec7b0a30d999
jeopardyKing <bhararaaayush@gmail.com>
2021-09-04 19:27:44 +0530

    COMMIT

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var stone;
+var iron;
+var rubber;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    stone = new Stone(300,100,50,50);
+    iron = new Iron(400,200,100,100);
+    rubber = new Rubber(500,300,150);
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+    plane.display();
+    hammer.display();
+    stone.display();
+    iron.display();
+    rubber.display();
+ }
